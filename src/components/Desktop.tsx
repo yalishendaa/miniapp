a4ab8386604af6112584cc838cfdda105ea89cc8
Alex Yava <scalpinkap@gmail.com>
2025-10-26 17:38:46 +0300

    uU

--- src/components/Desktop.tsx
+++ src/components/Desktop.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useXp } from "../hooks/useXp";
-import { usePopups } from "../hooks/UsePopups";
+import { usePopups } from "../hooks/usePopups";
 import { useDraggable } from "../hooks/useDraggable";
 
 import DesktopIcon from "./DesktopIcon";
